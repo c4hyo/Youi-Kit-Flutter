c0f4505c21d6707e1e39808b7f0191469172268e
c4hyo <mdcnugroho@student.ce.undip.ac.id>
2022-10-06 07:27:36 +0700

    app route

---
+++ lib/router/app_router.dart
@@ -0,0 +1,9 @@
+part of "app_page.dart";
+
+abstract class Routes {
+  static const CHAT_LOGIN = "/chat-login";
+  static const CHAT_REGISTER = "/chat-register";
+  static const CHAT_LIST = "/chat-list";
+  static const CHAT_ROOM = "/chat-room";
+  static const CHAT_PROFILE = "/chat-profile";
+}
